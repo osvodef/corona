c0e22dfbca35b9a585d1e5f47fa9d672c168569c
Alex Fedosov <osvodef@gmail.com>
2020-03-25 02:38:09 +0700

    add no webgl warning

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,55 @@ import { buildModel } from './model';
 import { App } from './components/app';
 import { mapAttribution, initialBounds, initialBearing, initialPitch, mapStyle } from './constants';
 
-if (isMobile) {
-    document.body.classList.add('mobile');
+if (isSupported()) {
+    init();
+} else {
+    (document.getElementById('no-webgl') as HTMLDivElement).style.display = 'table';
 }
 
-const map = new mapbox.Map({
-    accessToken:
-        'pk.eyJ1Ijoib3N2b2RlZiIsImEiOiJjazNwbjNlMWUwNGtkM2Vtb253MjM3cXhvIn0.A9Qebgu0gf2BlndYixeeOw',
-    container: document.querySelector('.map') as HTMLDivElement,
-    style: mapStyle,
-    customAttribution: mapAttribution,
-    bearing: initialBearing,
-    pitch: initialPitch,
-    bounds: initialBounds,
-    fitBoundsOptions: {
-        padding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: !isNarrowScreen() ? 480 : 0,
+function init() {
+    if (isMobile) {
+        document.body.classList.add('mobile');
+    }
+
+    const map = new mapbox.Map({
+        accessToken:
+            'pk.eyJ1Ijoib3N2b2RlZiIsImEiOiJjazNwbjNlMWUwNGtkM2Vtb253MjM3cXhvIn0.A9Qebgu0gf2BlndYixeeOw',
+        container: document.querySelector('.map') as HTMLDivElement,
+        style: mapStyle,
+        customAttribution: mapAttribution,
+        bearing: initialBearing,
+        pitch: initialPitch,
+        bounds: initialBounds,
+        fitBoundsOptions: {
+            padding: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: !isNarrowScreen() ? 480 : 0,
+            },
         },
-    },
-    logoPosition: 'top-right',
-    renderWorldCopies: false,
-});
+        logoPosition: 'top-right',
+        renderWorldCopies: false,
+    });
 
-Promise.all([
-    fetch('data/cases.csv').then((response) => response.text()),
-    fetch('data/deaths.csv').then((response) => response.text()),
-]).then((results) => {
-    const model = buildModel(results[0], results[1]);
+    Promise.all([
+        fetch('data/cases.csv').then((response) => response.text()),
+        fetch('data/deaths.csv').then((response) => response.text()),
+    ]).then((results) => {
+        const model = buildModel(results[0], results[1]);
 
-    new App(document.body, map, model);
-});
+        new App(document.body, map, model);
+    });
+}
+
+function isSupported(): boolean {
+    try {
+        return (
+            'WebGLRenderingContext' in window &&
+            !!document.createElement('canvas').getContext('webgl')
+        );
+    } catch (e) {
+        return false;
+    }
+}
