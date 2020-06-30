cb0be3a15b72776dab30b65572539a878fc3128d
Alex Fedosov <osvodef@gmail.com>
2020-06-30 14:45:35 +0700

    remove detailed russian data

--- scripts/types.ts
+++ scripts/types.ts
@@ -2,14 +2,3 @@ export interface Record {
     [key: string]: string;
 }
 export type ParsedCsv = Record[];
-
-export type RussianRecord = {
-    name: string;
-    confirmed: number[];
-    dead: number[];
-};
-
-export type RussianData = {
-    startDate: string;
-    data: RussianRecord[];
-};
